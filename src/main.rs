ff7862435f9583a9cbbf22d217894409a25dc20a
youpie <youplamb@hotmail.nl>
2025-02-25 20:41:41 +0100

    updated to allow user to just drop files

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,70 @@
 // main.rs
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::path::PathBuf;
-use qpdf::QPdf;
 use lopdf::Document;
+use qpdf::QPdf;
 use regex::Regex;
-use std::fs;
+use serde::{Deserialize, Serialize};
+use shift_indexing::read_pdf_stream;
+use std::collections::HashMap;
 use std::error::Error;
+use std::fs::{self};
+use std::path::PathBuf;
+use std::sync::LazyLock;
+use std::sync::RwLock;
+use time::macros::format_description;
+use time::{Date, OffsetDateTime};
+
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
 
 pub mod shift_indexing;
 
-const PDF_PATH: &str = "./Dienstboek/Dienstboekjes_Gecombineerd.pdf";
+const PDF_PATH: &str = "./Dienstboek/";
+
+static NEW_TIMETABLE: LazyLock<RwLock<Option<Date>>> = LazyLock::new(|| RwLock::new(None));
+static CURRENT_COLLECTION: LazyLock<RwLock<PdfCollection>> = LazyLock::new(|| RwLock::new(PdfCollection::new()));
 
 pub type GenResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 struct ShiftData {
     pages: Vec<u32>,
-    // pdf_id: u32
+    file_id: usize,
 }
 
 struct ShiftMap {
     shifts: HashMap<String, ShiftData>,
 }
 
-struct PDFMap {
-    pdf_id: HashMap<u32, u32>
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct PdfCollection {
+    valid_from: Date,
+    files: HashMap<usize,String>,
+    pages: HashMap<String, ShiftData>,
 }
 
-// struct PDFSheet {
-//     name: String,
-//     valid_date_start: chrono::DateTime<>
+impl PdfCollection {
+    fn new() -> Self {
+        Self {
+            valid_from: Date::from_iso_week_date(2000, 20, time::Weekday::Monday).unwrap(),
+            files: HashMap::new(),
+            pages: HashMap::new(),
+        }
+    }
+}
+
+// impl Default for Date {
+//     fn default() -> Self {
+//
+//     }
 // }
 
-
-/// This function loads the PDF, searches for the trip number on each page,
-/// and writes the index to a JSON file.
-fn index_trip_sheets(pdf_path: PathBuf, output_path: &str) -> Result<(), Box<dyn Error>> {
+// Load every PDF and group them
+fn index_trip_sheets(pdf_path: PathBuf, file_id: usize) -> Result<(), Box<dyn Error>> {
     // Load the PDF document.
-    let doc = Document::load(pdf_path)?;
+    let doc = Document::load(pdf_path.clone())?;
 
     // Define a regex pattern that finds "Dienst" followed by a trip number.
     let re = Regex::new(r"Dienst\s*(\b[A-Z]{1,2} \d{4}\b)")?;
@@ -57,51 +82,110 @@ fn index_trip_sheets(pdf_path: PathBuf, output_path: &str) -> Result<(), Box<dyn
             let trip_number = cap.get(1).map_or("", |m| m.as_str()).to_string();
 
             // Add the page number to the index for this trip number.
-            index.entry(trip_number)
+            index
+                .entry(trip_number)
                 .and_modify(|pages| pages.pages.push(*page_num))
-                .or_insert(ShiftData{pages: vec![*page_num]});
+                .or_insert(ShiftData {
+                    pages: vec![*page_num],
+                    file_id,
+                });
+        }
+    }
+    let date_format = format_description!["[day]-[month]-[year]"];
+    let valid_from_string = read_pdf_stream(pdf_path.clone())?;
+    println!("{valid_from_string}");    
+    let valid_from_day = time::Date::parse(&valid_from_string, date_format).unwrap();
+
+    let pdf_filename = pdf_path.file_name().unwrap().to_string_lossy();
+    let pdf_collection_output: PdfCollection;
+    if let Ok(file) = fs::read(format!("./pdf_collection/{}", valid_from_string)) {
+        let mut pdf_collection: PdfCollection = serde_json::from_slice(&file)?;
+        pdf_collection.files.insert(file_id,pdf_filename.to_string());
+        pdf_collection.pages.extend(index);
+        pdf_collection_output = pdf_collection;
+    } else {
+        pdf_collection_output = PdfCollection {
+            valid_from: valid_from_day,
+            files: HashMap::from([(file_id,pdf_filename.to_string())]),
+            pages: index,
         }
     }
 
     // Serialize the index into pretty JSON.
-    let index_json = serde_json::to_string_pretty(&index)?;
-    fs::write(output_path, index_json)?;
-    println!("Index saved to {}", output_path);
+    let output_path = PathBuf::from(format!("./pdf_collection/{}", valid_from_string));
+    let index_json = serde_json::to_string_pretty(&pdf_collection_output)?;
+    fs::write(&output_path, index_json)?;
+    println!("Index saved to {:?}", &output_path);
 
     Ok(())
 }
 
-
 fn load_shifts() -> Result<HashMap<String, ShiftData>, Box<dyn std::error::Error>> {
     let data = std::fs::read_to_string("./trip_index.json")?;
     let raw_shifts: HashMap<String, ShiftData> = serde_json::from_str(&data)?;
-    
+
     // Normalize keys by removing spaces
     let shifts = raw_shifts
         .into_iter()
         .map(|(k, v)| (k.replace(' ', ""), v))
         .collect();
-    
+
     Ok(shifts)
+}
+
+// let current_date = OffsetDateTime::now_utc();
+
+// load all pdf_collection files. And determine which one is current
+// Also if it exists, save the date of when it gets invalidated (Next timetable)
+fn get_valid_timetable() -> GenResult<(PdfCollection, Option<Date>)> {
+    let collections = fs::read_dir("pdf_collection")?;
+    let current_date = OffsetDateTime::now_utc().date();
+    let mut latest_collection = PdfCollection::new();
+    let mut next_timetable: Option<Date> = None;
+    for file_result in collections {
+        let file = file_result?;
+        let current_collection_file: PdfCollection =
+            serde_json::from_slice(&fs::read(file.path())?)?;
+        if current_collection_file.valid_from > latest_collection.valid_from
+            && current_collection_file.valid_from <= current_date
+        {
+            latest_collection = current_collection_file;
+        } else if current_collection_file.valid_from > current_date {
+            next_timetable = Some(current_collection_file.valid_from);
+        }
+    }
+
+    fs::write("new_timetable", serde_json::to_string(&next_timetable)?)?;
+    Ok((latest_collection, next_timetable))
 }
 
 #[get("/shift/{shift_number}")]
 async fn get_shift(
     shift_number: web::Path<String>,
-    data: web::Data<ShiftMap>,
 ) -> impl Responder {
     // Normalize input by removing spaces
-    let normalized = shift_number.replace(' ', "");
-    let normalized = normalized.to_uppercase();
-    let pdf = QPdf::read(PDF_PATH).unwrap(); 
-    let shift_data = match data.shifts.get(&normalized) {
-        Some(data) => data,
+    let normalized_shift_number = shift_number.replace(' ', "");
+    let normalized_shift_number = normalized_shift_number.to_uppercase();
+    if let Some(new_timetable_date) = *NEW_TIMETABLE.read().unwrap() {
+        if new_timetable_date >= OffsetDateTime::now_utc().date() {
+            let _ = fs::read_dir("Dienstboek").unwrap()
+                .into_iter()
+                .enumerate()
+                .map(|path| index_trip_sheets(path.1.unwrap().path(), path.0).unwrap())
+                .collect::<Vec<_>>();
+            *CURRENT_COLLECTION.write().unwrap() = get_valid_timetable().unwrap().0;
+        }
+    }
+    let current_collection = CURRENT_COLLECTION.read().unwrap().clone();
+    let (shift_path, shift_page) = match current_collection.pages.get(&normalized_shift_number) {
+        Some(data) => {(current_collection.files.get(&data.file_id).unwrap(), data.pages.clone())},
         None => return HttpResponse::NotFound().body("<h1>Deze dienst is niet gevonden!</h1>"),
     };
+    let pdf = QPdf::read(format!("{PDF_PATH}/{shift_path}")).unwrap();
     let new_doc = QPdf::empty();
-    
+
     // Keep only the pages we want
-    let extracted_pages = pdf.get_page(*shift_data.pages.last().unwrap()-1).unwrap();
+    let extracted_pages = pdf.get_page(*shift_page.last().unwrap() - 1).unwrap();
     new_doc.add_page(extracted_pages, true).unwrap();
     let bytes = new_doc.writer().write_to_memory().unwrap();
 
@@ -110,32 +194,33 @@ async fn get_shift(
         .body(bytes)
 }
 
-fn load_directory(path: PathBuf) -> PathBuf {
-    let paths = fs::read_dir(&path).unwrap();
-    if paths.into_iter().count() == 1{
-        fs::read_dir(path).unwrap().last().unwrap().unwrap().path()
-    }
-    else {
-        panic!("Er moet EEN item in het dienstboekje folder zitten")
-    }
-
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    pretty_env_logger::init();
     // Load shift data
-    let pdf_path = load_directory(PathBuf::from("./Dienstboek"));
-    index_trip_sheets(pdf_path.clone(), "trip_index.json").unwrap();
-    let shifts = load_shifts().expect("Failed to load shifts");
-    shift_indexing::read_pdf_stream(pdf_path).unwrap();
-    let app_state = web::Data::new(ShiftMap { shifts });
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(app_state.clone())
-            .service(get_shift)
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    // Load when the new timetable goes into effect
+    // *new_timetable.lock().unwrap() = match fs::read("new_timetable") {
+    //     Ok(date_stream) => match serde_json::from_slice::<Option<Date>>(&date_stream) {
+    //         Ok(date) => date,
+    //         Err(_) => get_valid_collection().unwrap().1,
+    //     },
+    //     Err(_) => get_valid_collection().unwrap().1,
+    // };
+    let current_timetable = get_valid_timetable().unwrap();
+    *CURRENT_COLLECTION.write().unwrap() = current_timetable.0;
+    *NEW_TIMETABLE.write().unwrap() = current_timetable.1;
+    println!("Indexing trip sheets");
+    let _ = fs::read_dir("Dienstboek")?
+        .into_iter()
+        .enumerate()
+        .map(|path| index_trip_sheets(path.1.unwrap().path(), path.0).unwrap())
+        .collect::<Vec<_>>();
+    //let shifts = load_shifts().expect("Failed to load shifts");
+    //shift_indexing::read_pdf_stream(pdf_path).unwrap();
+    //let app_state = web::Data::new(current_timetable.0);
+    //.app_data(dfsij)
+    HttpServer::new(move || App::new().service(get_shift))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
--- src/shift_indexing.rs
+++ src/shift_indexing.rs
@@ -1,69 +1,89 @@
-use chrono::NaiveTime;
-use lopdf::Document;
+#![allow(warnings)]
 use crate::GenResult;
+use lopdf::Document;
+use regex::Regex;
 use std::path::PathBuf;
+use time::macros::format_description;
+use time::{error, format_description, Time};
+
+trait StrTime {
+    fn string_to_time(&self) -> Result<Time, error::Parse>;
+}
+
+impl StrTime for String {
+    fn string_to_time(&self) -> Result<Time, error::Parse> {
+        let format = format_description!("[hour]:[minute]");
+        Ok(Time::parse(self, format)?)
+    }
+}
 
 enum ShiftValid {
     Weekdagen,
     Zaterdag,
-    Zondag
+    Zondag,
 }
 
 enum ShiftType {
     Vroeg,
     Tussen,
-    Gebroken{start_break: Option<NaiveTime>, end_break: Option<NaiveTime>}, // If one is none, it means it's half of a broken shift
-    Laat
+    Gebroken {
+        start_break: Option<Time>,
+        end_break: Option<Time>,
+    }, // If one is none, it means it's half of a broken shift
+    Laat,
 }
 
-enum JobDrivingType{
+enum JobDrivingType {
     Lijn(u32),
     Mat,
 }
 
-enum JobMessageType{
-    Meenemen{dienstnummers: Vec<u32>},
-    Passagieren{dienstnummer: u32, omloop: u32},
-    BusOp{lijn: u32},
-    NeemBus{bustype: String},
+enum JobMessageType {
+    Meenemen { dienstnummers: Vec<u32> },
+    Passagieren { dienstnummer: u32, omloop: String },
+    BusOp { lijn: u32 },
+    NeemBus { bustype: String },
     Other(String),
 }
 
 enum JobType {
-    Rijden{drive_type: JobDrivingType},
-    Pauze{onderbreking: bool},
+    Rijden { drive_type: JobDrivingType },
+    Pauze,
+    Onderbreking,
     OpAfstap,
     RijklaarMaken,
     StallenAfmelden,
-    Melding{message: JobMessageType}
+    Melding { message: JobMessageType },
+    Temp,
 }
 
 struct ShiftJob {
     job_type: JobType,
-    start: Option<NaiveTime>,
-    end: Option<NaiveTime>,
+    start: Option<Time>,
+    end: Option<Time>,
     start_location: Option<String>,
-    end_location: Option<String>,    // If none, it's the same as start
-    omloop: Option<u32>
+    end_location: Option<String>, // If none, it's the same as start
+    omloop: Option<u32>,
+    rit: Option<u32>,
 }
 
 struct Shift {
     shift_nr: String,
-    start_time: chrono::NaiveTime,
-    end_time: chrono::NaiveTime,
+    start_time: Time,
+    end_time: Time,
     valid_on: ShiftValid,
     location: String,
     shift_type: ShiftType,
-    job: Vec<ShiftJob>
+    job: Vec<ShiftJob>,
 }
 
-pub fn read_pdf_stream(pdf_path: PathBuf) -> GenResult<()> {
+pub fn read_pdf_stream(pdf_path: PathBuf) -> GenResult<String> {
     let doc = Document::load(pdf_path)?;
     let pages = doc.get_pages();
-    for (&page_number, &page_id) in pages.iter(){
+    for (&page_number, &page_id) in pages.iter() {
         let page_dict = doc.get_object(page_id)?.as_dict()?;
         let contents = page_dict.get(b"Contents")?;
-        println!("{:#?}",contents);
+        println!("{:#?}", contents);
         match contents {
             lopdf::Object::Reference(r) => {
                 let object = doc.get_object(*r)?.as_stream()?;
@@ -75,13 +95,131 @@ pub fn read_pdf_stream(pdf_path: PathBuf) -> GenResult<()> {
                 let stream_string = stream_string.replace("Tj", "");
                 let stream_string = stream_string.replace("Tf", "");
                 // let stream = lopdf::Object::Stream(*object);
-                println!("Page {} stream: {}", page_number, stream_string);
+                //println!("Page {} stream: {}", page_number, stream_string);
+                return Ok(find_text_and_coordinate(stream_string)?.first().unwrap().0.clone());
             }
             _ => {
                 println!("Unexpected type for Contents on page {}", page_number);
             }
         }
         break;
+    }
+    Ok("Nothing found".to_string())
+}
+
+fn find_text_and_coordinate(page_stream: String) -> GenResult<Vec<(String, f32)>> {
+    let re = Regex::new(r"\((.*?)\)").unwrap(); // Match text inside parentheses
+    let mut line_elements: Vec<(String, (f32, f32))> = vec![];
+    for (line_number, line) in page_stream.clone().lines().enumerate() {
+        for cap in re.captures_iter(line) {
+            let mut coordinate_split = page_stream
+                .lines()
+                .nth(line_number - 1)
+                .unwrap()
+                .split_ascii_whitespace();
+            let coordinate: (f32, f32) = (
+                coordinate_split.next().unwrap().parse().unwrap(),
+                coordinate_split.next().unwrap().parse().unwrap(),
+            );
+
+            println!(
+                "Line {}: {} op positie {:?}",
+                line_number + 1,
+                &cap[1],
+                coordinate
+            );
+            line_elements.push((cap[1].to_string(), coordinate));
+            if line.contains("Ingangsdatum"){
+                let line = &cap[1];
+                println!("Line: {line}");
+                let start_date = line.split("Ingangsdatum ").last().unwrap();
+                return Ok(vec![(start_date.to_string(),0.0)]);
+            }
+        }
+    }
+    println!("{}", line_elements.len());
+    get_line_element(line_elements)?;
+    Ok(vec![("sad".to_string(), 1.0)])
+}
+
+fn get_line_element(items: Vec<(String, (f32, f32))>) -> GenResult<()> {
+    let mut last_y = items.first().unwrap().1 .1;
+    let mut lijn: Option<_> = None;
+    let mut omloop: Option<_> = None;
+    let mut rit: Option<_> = None;
+    let mut start: Option<_> = None;
+    let mut van: Option<_> = None;
+    let mut naar: Option<_> = None;
+    let mut eind: Option<_> = None;
+    for item in items {
+        if last_y != item.1 .1 {
+            println!("Job gevonden!\nLijn {lijn:?}, omloop {omloop:?}, rit {rit:?}, van {van:?}, naar {naar:?}, begint om {start:?} en stopt om {eind:?}");
+            lijn = None;
+            omloop = None;
+            rit = None;
+            start = None;
+            van = None;
+            naar = None;
+            eind = None;
+        }
+        match item.1 .0 {
+            83.0..=92.0 => lijn = Some(item.0),
+            200.0..=280.0 => omloop = Some(item.0),
+            300.0..=350.0 => rit = Some(item.0),
+            350.0..=390.0 => start = Some(item.0),
+            400.0..=420.0 => van = Some(item.0),
+            450.0..=480.0 => naar = Some(item.0),
+            490.0.. => eind = Some(item.0),
+            _ => (),
+        }
+        last_y = item.1 .1;
+    }
+    Ok(())
+}
+
+fn job_creator(
+    lijn: Option<String>,
+    omloop: Option<String>,
+    rit: Option<String>,
+    start: Option<String>,
+    eind: Option<String>,
+    van: Option<String>,
+    naar: Option<String>,
+) -> GenResult<()> {
+    let job_drive_type;
+    let job_type;
+    if let Some(lijn_string) = lijn {
+        if lijn_string == "MAT" {
+            job_drive_type = Some(JobDrivingType::Mat);
+        } else if let Ok(lijn_parse) = lijn_string.parse::<u32>() {
+            job_drive_type = Some(JobDrivingType::Lijn(lijn_parse));
+        } else {
+            job_drive_type = None;
+            let lijn_first_word = lijn_string.split_whitespace().next().unwrap().to_lowercase();
+            let first_word_str = lijn_first_word.as_str();
+            let message = match lijn_first_word.as_str() {
+                "neem" => JobMessageType::NeemBus {
+                    bustype: lijn_string.replace("neem ", ""),
+                },
+                "bus" => JobMessageType::BusOp {
+                    lijn: lijn_string.replace("Bus op lijn ", "").parse()?,
+                },
+                "pod" => JobMessageType::NeemBus {
+                    bustype: lijn_string,
+                },
+                "pass" => {
+                    let lijn_string_split = lijn_string.replace("Pass met ", "");
+                    let mut dienst_omloop_split = lijn_string_split.split_whitespace().next().unwrap().split('/');
+                    JobMessageType::Passagieren {
+                        dienstnummer: dienst_omloop_split.next().unwrap().parse().unwrap(),
+                        omloop: dienst_omloop_split.next().unwrap().to_string(),
+                    }
+                }
+                "meenemen" => JobMessageType::Other(lijn_string),
+                _ => JobMessageType::Other(lijn_string),
+            };
+            job_type = message;
+        }
     }
     Ok(())
 }
